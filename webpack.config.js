a371b29ceb46d099b19bbb3ca99c786da6c66cc7
tokei <tokei@pixiv.co.jp>
2021-10-13 01:20:13 +0900

    最初の準備

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/main.ts",
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
